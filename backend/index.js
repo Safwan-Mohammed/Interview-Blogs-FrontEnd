56c297c2519a648ca3ff37f1a72a284576edefdd
anfal17 <mohdanfal17@gmail.com>
2024-08-20 14:17:44 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const multer = require('multer') 
+
+dotenv.config()
+app.use(express.json())
+
+app.use(cors())
+const corsOptions = {
+    origin:'*',
+    credential:true,
+}
+
+app.use(cors(corsOptions))
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("Database Connection Esatblished ");
+        
+    }catch(error){
+        console.log("Index file DB error ", error);
+        
+    }
+}
+
+app.listen(process.env.PORT , ()=>{
+    connectDB()
+    console.log("App is running on port"+process.env.PORT);
+    
+})
