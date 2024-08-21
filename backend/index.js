f0ed1d90a9b11b56fcc49da88d2e2296b334a8b9
anfal17 <mohdanfal17@gmail.com>
2024-08-21 19:33:29 +0530

    minor fixes

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ const corsOptions = {
     credentials: true,               // Allow credentials
 };
 
+app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 
 app.use(express.json());
