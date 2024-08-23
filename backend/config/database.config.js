9bcb1b3fef5373df9d52beba455d3c9a29ce632e
anfal17 <mohdanfal17@gmail.com>
2024-08-23 20:20:31 +0530

    still fix

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect('mongodb+srv://mohdanfal17:8876@cluster0.jwbhg.mongodb.net/')
         console.log("Database Connection Esatblished ");
         
     }catch(error){
