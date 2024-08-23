ff1ede63c5f56a45efddb0f8f0dea8b892696bc9
anfal17 <mohdanfal17@gmail.com>
2024-08-23 18:59:19 +0530

    test cors

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
     withCredentials: true,
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://interview-blogs-api.vercel.app'
 })
 
 export default axiosInstance
