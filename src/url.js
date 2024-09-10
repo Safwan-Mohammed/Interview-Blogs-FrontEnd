5356f5dbf835d9fa7c2fd770ae55d281ea7feb33
Safwan-Mohammed <safwanhanif19@gmail.com>
2024-09-10 15:47:56 +0530

    Jenkins file updated

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const URL = import.meta.env.VITE_ENVIRONMENT === 'PROD' ? `URL` : `http://localhost:8000`
-export const IF = import.meta.env.VITE_ENVIRONMENT === 'PROD' ? `URL/images` : `http://localhost:8000/images`
+export const URL = import.meta.env.VITE_ENVIRONMENT === 'PROD' ? `interview-blogs-backend-production.up.railway.app` : `http://localhost:8000`
+export const IF = import.meta.env.VITE_ENVIRONMENT === 'PROD' ? `interview-blogs-backend-production.up.railway.app/images` : `http://localhost:8000/images`
