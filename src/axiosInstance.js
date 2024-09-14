02936e9a5cad899cf1e49a74501b739fa6b81917
Safwan-Mohammed <safwanhanif19@gmail.com>
2024-09-14 23:19:52 +0530

    Updates

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
     withCredentials: true,
-    baseURL: import.meta.env.VITE_ENVIRONMENT === 'PROD' ? `interview-blogs-backend-production.up.railway.app` : `http://localhost:8000`
+    baseURL: import.meta.env.VITE_ENVIRONMENT === 'PROD' ? `https://backend.safwanspehere.online` : `http://localhost:8000`
 })
 
 export default axiosInstance
